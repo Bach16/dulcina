333a4a88ee819af75fccf9ba7a3ad4334d849cc9
daveless <44807207+Daveless@users.noreply.github.com>
2024-09-16 23:44:28 -0500

    update mobile version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         </div>
       </header>
       <main>
-        <div className="main-bg bg-fixed flex w-[100%]">
+        <div className="main-bg bg-fixed flex w-[100%] h-screen">
           <div className="w-[100%] bg-[#FCE8EA]/[0.7] backdrop-brightness-75">
             <div className="h-[85vh] flex flex-col items-center text-center text-[#EC174F]">
               <div className="flex flex-col items-center justify-between h-[14rem] mt-[15vh]">
@@ -27,15 +27,15 @@ export default function Home() {
                   <h2 className="text-[1.2rem]">
                     El detalle perfecto para alegrar cualquier ocasión especial
                   </h2>
-                  <h1 className="text-[5rem] helvetica-rounded">
+                  <h1 className="text-[4rem] helvetica-rounded">
                     REGALOS IDEALES
                   </h1>
                 </div>
-                <div className="flex justify-between w-[30rem]">
-                  <button className="px-[2rem] py-[0.8rem] rounded-full bg-[#EC174F] text-[#ffffff]">
+                <div className="flex justify-between flex-col gap-5 w-5/6 mt-5">
+                  <button className="px-[2rem] font-extrabold py-[0.8rem] rounded-full bg-[#EC174F] text-[#ffffff] border-[2px] border-[#EC174F] border-solid">
                     Cotiza tu regalo
                   </button>
-                  <button className="px-[2rem] py-[0.8rem] rounded-full font-bold border-[2px] border-[#EC174F] border-solid">
+                  <button className="px-[2rem] font-extrazbold py-[0.8rem] rounded-full  border-[2px] border-[#EC174F] border-solid">
                     Mira nuestros productos
                   </button>
                 </div>
@@ -43,19 +43,19 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="w-[100%] circle-mask pt-[7rem] flex flex-col gap-[4rem] justify-center items-center">
+        <div className="w-[100%] circle-mask pt-[7rem] flex flex-col gap-3  justify-center items-center p-7">
           <div className="text-[#EC174F] flex flex-col justify-center items-center">
             <h2 className="text-[1.2rem]">
               El detalle para cualquier ocasión especial
             </h2>
-            <h1 className="text-[3.5rem] helvetica-rounded">REGALOS IDEALES</h1>
+            <h1 className="text-[3.9rem] helvetica-rounded text-left leading-none">REGALOS IDEALES</h1>
           </div>
-          <div className="flex justify-between w-[75%]">
+          <div className="w-full grid grid-cols-2 gap-2">
             {data.map((e: any) => {
               return <Product product={e} key={e.id} />;
             })}
           </div>
-          <div className="flex justify-between w-[75%]">
+          <div className="w-full grid grid-cols-2 gap-2">
             {data1.map((e: any) => {
               return <Product product={e} key={e.id} />;
             })}
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Product:FC<Props> = ({product}) => {
   return (
-    <div id="product-container" className="text-[#EC174F] w-[220px] ">
+    <div id="product-container" className="text-[#EC174F] w-full p-3">
       <div className="overflow-hidden">
         <div>
           <img src={product?.image} alt={product?.name} id="product-mask" className=" h-[19rme] w-[19rem]"/>
@@ -15,11 +15,11 @@ const Product:FC<Props> = ({product}) => {
       </div>
       <div className="flex flex-col items-center">
         <div>
-            <h3 className="text-[1.35rem] font-bold ">{product?.name}</h3>
+            <h3 className="text-[1.3rem] font-bold leading-tight mt-3">{product?.name}</h3>
             <p className="text-[1rem]">$ {product?.price}</p>
         </div>
         <a className="mt-[0.6rem] w-[101%] " target="_blank" rel="noopener noreferrer" href={product?.link}>
-        <button id="product-button" className="border-solid font-semibold border-[2px] border-[#EC174F] w-[100%] rounded-full py-[0.5rem]">Comprar</button>
+        <button id="product-button" className="border-solid font-semibold border-[2px] border-[#EC174F] w-[100%] rounded-[1.5rem] py-[0.5rem]">Comprar</button>
         </a>
       </div>
     </div>
