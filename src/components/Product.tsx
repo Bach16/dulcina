4ae93acfdca7c7025ba832f7fd796394a9d65173
Bach16 <gwyn56717@gmail.com>
2025-06-25 20:37:15 -0500

    feat: change walink products

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -34,7 +34,7 @@ const Product:FC<Props> = ({product}) => {
           className="mt-[0.6rem] w-[101%] "
           target="_blank"
           rel="noopener noreferrer"
-          href={product?.image}
+          href={product?.description}
         >
           <button
             id="product-button"
